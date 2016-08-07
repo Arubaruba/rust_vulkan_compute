47d016651dd07538bc68ec46735f1fc77d3d02c5
Andreas Stocker <shadowstep7@gmail.com>
2016-08-07 14:55:33 +0200

    initial working compute shader!

--- src/compute.glsl
+++ src/compute.glsl
@@ -1,5 +1,14 @@
 #version 450
 
+#extension GL_ARB_separate_shader_objects : enable
+#extension GL_ARB_shading_language_420pack : enable
+
+layout(set = 0, binding = 0) buffer Data {
+    uint asdf;
+} uniforms;
+
 void main()
 {	
+    
+    uniforms.asdf = uniforms.asdf + 900000;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 #[macro_use]
 extern crate vulkano;
 
-use vulkano::buffer::BufferUsage;
 use vulkano::buffer::CpuAccessibleBuffer;
 use vulkano::command_buffer;
 use vulkano::command_buffer::PrimaryCommandBufferBuilder;
-use vulkano::command_buffer::Submission;
-use vulkano::descriptor::pipeline_layout::EmptyPipeline;
 use vulkano::device::Device;
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
 use vulkano::pipeline::ComputePipeline;
 
 use std::sync::Arc;
+use std::time::Duration;
 
 fn main() {
-    let instance = Instance::new(None, &InstanceExtensions::none(), None).expect("failed to create Vulkan instance");
+    let instance = Instance::new(None, &InstanceExtensions::none(), None)
+        .expect("failed to create Vulkan instance");
 
     let physical = vulkano::instance::PhysicalDevice::enumerate(&instance)
         .next()
@@ -27,7 +26,7 @@ fn main() {
              physical.ty());
 
     let queue = physical.queue_families()
-        .find(|q| q.supports_graphics())
+        .find(|q| q.supports_compute())
         .expect("couldn't find a graphical queue fmy");
 
     let (device, mut queues) = {
@@ -43,45 +42,48 @@ fn main() {
             .expect("failed to create device")
     };
 
-    // Since we can request multiple queues, the `queues` variable is in fact an iterator. In this
-    // example we use only one queue, so we just retreive the first and only element of the
-    // iterator and throw it away.
     let queue = queues.next().unwrap();
 
-    // We now create a buffer that will store the shape of our triangle.
-    let vertex_buffer = {
-        #[derive(Debug, Clone)]
-        struct Vertex {
-            position: [f32; 2],
-        }
-        impl_vertex!(Vertex, position);
-
-        CpuAccessibleBuffer::from_iter(&device,
-                                       &BufferUsage::all(),
-                                       Some(queue.family()),
-                                       [Vertex { position: [-0.5, -0.25] },
-                                        Vertex { position: [0.0, 0.5] },
-                                        Vertex { position: [0.25, -0.1] }]
-                                           .iter()
-                                           .cloned())
-            .expect("failed to create buffer")
-    };
     mod cs {
         include!{concat!(env!("OUT_DIR"), "/shaders/src/compute.glsl")}
     }
     let cs = cs::Shader::load(&device).expect("failed to create shader module");
 
+    let storage_buffer = CpuAccessibleBuffer::from_data(&device,
+                                                        &vulkano::buffer::BufferUsage::all(),
+                                                        Some(queue.family()),
+                                                        1234usize)
+        .expect("failed to create buffer");
 
-    let pipeline = ComputePipeline::new(&device,
-                                        &EmptyPipeline::new(&device).unwrap(),
-                                        &cs.main_entry_point(),
-                                        &());
 
-    let mut submissions: Vec<Arc<Submission>> = Vec::new();
+    let descriptor_pool = vulkano::descriptor::descriptor_set::DescriptorPool::new(&device);
+    mod pipeline_layout {
+        pipeline_layout!{
+            set0: {
+                asdf: StorageBuffer<usize>
+            }
+        }
+    }
 
-    submissions.retain(|s| s.destroying_would_block());
+    println!("initial buffer val: {}",
+             *storage_buffer.read(Duration::new(1, 0)).unwrap());
 
-    let command_buffer = PrimaryCommandBufferBuilder::new(&device, queue.family()).build();
+    let pipeline_layout = pipeline_layout::CustomPipeline::new(&device).unwrap();
+    let set = pipeline_layout::set0::Set::new(&descriptor_pool,
+                                              &pipeline_layout,
+                                              &pipeline_layout::set0::Descriptors {
+                                                  asdf: &storage_buffer,
+                                              });
 
-    submissions.push(command_buffer::submit(&command_buffer, &queue).unwrap());
+    let pipeline = ComputePipeline::new(&device, &pipeline_layout, &cs.main_entry_point(), &())
+        .unwrap();
+
+    let command_buffer = PrimaryCommandBufferBuilder::new(&device, queue.family())
+        .dispatch(&pipeline, &set, [2, 1, 1], &())
+        .build();
+
+    command_buffer::submit(&command_buffer, &queue).unwrap().wait(Duration::new(1, 0)).unwrap();
+
+    println!("final buffer val: {}",
+             *storage_buffer.read(Duration::new(1, 0)).unwrap());
 }
